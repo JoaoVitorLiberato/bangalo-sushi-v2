e4f6e8a556df136a8fa2e5955e724424554ca1e6
João Vitor Liberato <j.liberato@vertex.vertexdigital.co>
2025-07-20 20:23:23 -0300

    Deixando apenas com a ladingpage, pois eu vou criar uma nova com vue 3

--- src/plugins/router/index.ts
+++ src/plugins/router/index.ts
@@ -4,12 +4,10 @@ import { titleUpdate } from "@/plugins/router/functions/titleUpdate"
 
 import "@/plugins/router/routerRegisterHooks"
 import mainRouter from "@/plugins/router/mainRouter"
-import productsRouter from "@/plugins/router/productsRouter"
 
 Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [].concat(
-  productsRouter as never[],
   mainRouter as never[],
 )
 
@@ -21,19 +19,6 @@ const router = new VueRouter({
 
 router.beforeEach((to, from, next) => {
   try {
-    if (/^(admin-details-view)$/i.test(String(to.name))) {
-      const PERMISION_USER =  sessionStorage.getItem("permision-user")
-
-      if (PERMISION_USER && to.params.session === undefined) to.params.permision = PERMISION_USER
-      if (to.params.session === undefined) to.params.session = "pedidos"
-    }
-
-    if (/^(products-view)$/i.test(String(to.name))) {
-      if (!/^delivery$/i.test(String(to.params.type))) {
-        router.push({ name: "products-view", params: { type: "delivery" } })
-      }
-    }
-
     titleUpdate(to)
   } catch {/* EMPTY */}
   next()
